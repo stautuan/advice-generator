0718abad6e8064731baa0c2ee5f09416dfca0733
stautuan <stautuan89@gmail.com>
2024-05-21 00:26:12 -0400

    Implemented error handling and refactored code

--- script.js
+++ script.js
@@ -5,18 +5,30 @@ const adviceBtn = document.querySelector('.advice__btn');
 const adviceNumber = document.querySelector('.advice__number');
 const adviceText = document.querySelector('.advice__text');
 
+function showError(
+  message = 'Failed to fetch advice. Please try again later.'
+) {
+  adviceNumber.innerHTML = '';
+  adviceText.innerHTML = message;
+}
+
 async function getAdvice() {
   try {
     const response = await fetch('https://api.adviceslip.com/advice');
-    if (!response.ok)
-      throw new Error(`HTTP error! 💥 status: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     const data = await response.json();
     const advice = data.slip;
 
-    adviceNumber.innerHTML = `${advice.id}`;
-    adviceText.innerHTML = `"${advice.advice}"`;
+    if (advice) {
+      adviceNumber.innerHTML = `Advice #${advice.id}`;
+      adviceText.innerHTML = `"${advice.advice}"`;
+    } else {
+      showError();
+    }
   } catch (error) {
-    console.error(error);
+    showError();
   }
 }
 
